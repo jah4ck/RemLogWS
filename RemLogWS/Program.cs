4542ce6b23bf9aadce6aaca051fbb86044bce1d0
jah4ck <johann.simon.web@gmail.com>
2016-12-02 09:55:22 +0100

    valide

--- RemLogWS/Program.cs
+++ RemLogWS/Program.cs
@@ -141,7 +141,6 @@ namespace RemLogWS
                                                 }
                                                 catch (Exception err)
                                                 {
-
                                                 }
 
                                             }
